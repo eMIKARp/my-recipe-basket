be397f4d241a37c93c43ed4f636dc2767fc7f896
eMIKARp <emil.karpowicz@gmail.com>
2018-06-15 14:58:03 +0200

    Password encyption using MD5 algoryth

--- src/main/java/pl/mojprzepisnik/service/UserService.java
+++ src/main/java/pl/mojprzepisnik/service/UserService.java
@@ -1,5 +1,8 @@
 package pl.mojprzepisnik.service;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import pl.mojprzepisnik.dao.DaoFactory;
 import pl.mojprzepisnik.dao.UserDao;
 import pl.mojprzepisnik.model.User;
@@ -10,11 +13,26 @@ public class UserService {
         User user = new User();
         user.setUsername(username);
         user.setEmail(email);
-        user.setPassword(password);
+        String md5Pass = encryptPassword(password);
+        user.setPassword(md5Pass);
         user.setIsActive(true);
         DaoFactory factory = DaoFactory.getDaoFactory();
         UserDao userDao = factory.getUserDao();
         userDao.create(user);
+    }
+    
+    private String encryptPassword(String password){
+        MessageDigest digest = null;
+        try{
+            digest = MessageDigest.getInstance("MD5");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        
+        digest.update(password.getBytes());
+        String md5Password = new BigInteger(1, digest.digest()).toString(16);
+        return md5Password;
+        
     }
     
     public User getUserById(long user_id){
